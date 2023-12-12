09b9b08d3bf1774c0a5876a4c536a82b4315d685
paul barbaux <paul.barbaux@gmail.com>
2023-12-12 20:06:23 +0100

    initial commit

---
+++ src/contentScript.js
@@ -0,0 +1,67 @@
+const pageTitle = document.head.getElementsByTagName("title")[0].innerHTML;
+console.log(
+  `Page title is: '${pageTitle}' - evaluated by Chrome extension's 'contentScript.js' file`
+);
+
+// Communicate with background file by sending a message
+chrome.runtime.sendMessage(
+  {
+    type: "GREETINGS",
+    payload: {
+      message: "Hello, my name is Con. I am from ContentScript.",
+    },
+  },
+  (response) => {
+    console.log(response.message);
+  }
+);
+
+// Listen for message
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.type === "COUNT") {
+    console.log(`Current count is ${request.payload.count}`);
+  }
+
+  sendResponse({});
+  return true;
+});
+
+window.addEventListener("load", function () {
+  let insert = false;
+  let currentURL = window.location.href;
+  currentURL = currentURL.replace(".com", ".dev");
+
+  let html = `
+  <style>
+  a:hover {
+    text-decoration: none;
+
+  }
+  </style>
+  <a href="${currentURL}" target="_blank" rel="noopener noreferrer" class="" style="" aria-label="Open this file in Web Vs Code">
+  <button type="button"   class="types__StyledButton-sc-ws60qy-0 hBMKRG" ><span data-component="buttonContent" class="Box-sc-g0xbh4-0 kkrdEu"><span data-component="leadingVisual" class="Box-sc-g0xbh4-0 trpoQ"><div class="Box-sc-g0xbh4-0 bSeflH"><svg aria-hidden="true" focusable="false" role="img" class="octicon octicon-code" viewBox="0 0 16 16" width="16" height="16" fill="currentColor" style="display: inline-block; user-select: none; vertical-align: text-bottom; overflow: visible;"><path d="m11.28 3.22 4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.749.749 0 0 1-1.275-.326.749.749 0 0 1 .215-.734L13.94 8l-3.72-3.72a.749.749 0 0 1 .326-1.275.749.749 0 0 1 .734.215Zm-6.56 0a.751.751 0 0 1 1.042.018.751.751 0 0 1 .018 1.042L2.06 8l3.72 3.72a.749.749 0 0 1-.326 1.275.749.749 0 0 1-.734-.215L.47 8.53a.75.75 0 0 1 0-1.06Z"></path></svg></div></span><span data-component="text">Web Vs Code</span></span></button></a>`;
+  let DocumentGet = document.querySelector('input[placeholder="Go to file"]');
+  let DocumentGet2 = document.querySelectorAll("span");
+  DocumentGet2.forEach((element) => {
+    if (element.innerText === "Go to file") {
+      if (!insert) {
+        console.log("extention span with title found");
+        element.parentElement.parentElement.parentElement.parentElement.parentElement.insertAdjacentHTML(
+          "beforeend",
+          html
+        );
+        insert = true;
+      }
+    }
+  });
+  // console.log(DocumentGet, DocumentGet2);
+  if (!insert) {
+    DocumentGet.parentElement.parentElement.parentElement.parentElement.parentElement.insertAdjacentHTML(
+      "beforeend",
+      html
+    );
+    insert = true;
+  }
+
+  console.log("page is fully loaded");
+});
